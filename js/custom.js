d65f8f3450e7f08cd0061b53ce1ace7bd23d0bbc
Gustavo Marques <gtooliveiramarques@gmail.com>
2018-09-30 01:55:44 -0300

    Criação do preloader da pagina

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,12 @@
-//Menu Mobile
+
+/* JQuery */
+
+/* Preloader */
+$('.preloader').show();
+$('.content').hide();
 $(document).ready(function(){
+    $('.preloader').hide();
+    $('.content').show();
+    /* Menu Mobile */
     $('.sidenav').sidenav();
-  });
+});
